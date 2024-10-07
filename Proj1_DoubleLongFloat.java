cb87a05cbb5cd87f91d52eb1e7970859dbf78ef4
Tia-beanie <144304264+Tia-beanie@users.noreply.github.com>
2024-10-06 17:24:19 -0700

    Add files via upload

---
+++ Proj1_DoubleLongFloat.java
@@ -0,0 +1,15 @@
+public class Proj1_DoubleLongFloat {
+    public static void main(String[] args){
+
+        int a = 9;
+        long b = 99999999999L; //has to include L
+        float c = 2.5F;  //has to include F
+        double d = 2.5; 
+
+        System.out.println("Integer a: " + a);
+        System.out.println("Long b: " + b);
+        System.out.println("Float c: " + c);
+        System.out.println("Double d: " +  d);
+
+    }
+}
